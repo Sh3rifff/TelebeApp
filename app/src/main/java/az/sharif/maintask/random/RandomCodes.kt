29145cb7543778685c02690e6f4e45e8a11f50f9
Serif <serifsireliyev2002@gmail.com>
2023-05-17 01:21:12 +0400

    All

---
+++ app/src/main/java/az/sharif/maintask/random/RandomCodes.kt
@@ -0,0 +1,37 @@
+package az.sharif.maintask.random
+
+class RandomCodes {
+
+    /**
+     *Register retrofit
+     */
+
+//    private fun retrofit() {
+//
+//        val call = RetrofitHelper.service.university()
+//
+//        call.enqueue(object : Callback<List<UniversityModel>> {
+//            override fun onResponse(
+//                call: Call<List<UniversityModel>>, response: Response<List<UniversityModel>>
+//            ) {
+//                if (response.isSuccessful) {
+//                    val university = response.body()
+//                    val def = "Select Uni"
+//                    val uni = listOf(def) + university!!.map { it.university }
+//                    val adapter = ArrayAdapter(
+//                        requireContext(),
+//                        R.layout.support_simple_spinner_dropdown_item,
+//                        uni
+//                    )
+//                    binding.spinner.adapter = adapter
+//                }
+//            }
+//
+//            override fun onFailure(call: Call<List<UniversityModel>>, t: Throwable) {
+//                t.printStackTrace()
+//            }
+//        })
+//    }
+
+
+}
